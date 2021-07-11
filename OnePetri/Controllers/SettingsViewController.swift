78ac0474299c2ded10bef4e8281e4d91bca6fa8f
Michael Shamash <michael@shamash.me>
2021-07-11 12:38:08 -0400

    Save settings to UserDefaults

--- OnePetri/Controllers/SettingsViewController.swift
+++ OnePetri/Controllers/SettingsViewController.swift
@@ -17,10 +17,25 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var appVersionLabel: UILabel!
     @IBOutlet weak var petriModelVersionLabel: UILabel!
     @IBOutlet weak var plaqueModelVersionLabel: UILabel!
+    
+    private var defaults:UserDefaults!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        defaults = UserDefaults.standard
+        
+        let petriConfThreshold = defaults.double(forKey: "PetriConfThreshold")
+        let petriIOUThreshold = defaults.double(forKey: "PetriIOUThreshold")
+        let plaqueConfThreshold = defaults.double(forKey: "PlaqueConfThreshold")
+        let plaqueIOUThreshold = defaults.double(forKey: "PlaqueIOUThreshold")
+        let plaqueNMSIOUThreshold = defaults.double(forKey: "PlaqueNMSIOUThreshold")
+        
+        petriConfTextField.text = (petriConfThreshold != 0.0 ? "\(petriConfThreshold)" : "")
+        petriIOUTextField.text = (petriIOUThreshold != 0.0 ? "\(petriIOUThreshold)" : "")
+        plaqueConfTextField.text = (plaqueConfThreshold != 0.0 ? "\(plaqueConfThreshold)" : "")
+        plaqueIOUTextField.text = (plaqueIOUThreshold != 0.0 ? "\(plaqueIOUThreshold)" : "")
+        plaqueNMSIOUTextField.text = (plaqueNMSIOUThreshold != 0.0 ? "\(plaqueNMSIOUThreshold)" : "")
         
         petriConfTextField.addDoneButtonToKeyboard(dismissAction: #selector(petriConfTextField.resignFirstResponder))
         petriIOUTextField.addDoneButtonToKeyboard(dismissAction: #selector(petriIOUTextField.resignFirstResponder))
@@ -33,4 +48,23 @@ class SettingsViewController: UIViewController {
         plaqueModelVersionLabel.text = "Plaque model version \(plaqueModelVersion)"
     }
 
+}
+
+
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField.tag == 0 {
+            if let value = textField.text, !value.isEmpty { defaults.set(Double(value), forKey: "PetriConfThreshold") } else { defaults.removeObject(forKey: "PetriConfThreshold") }
+        } else if textField.tag == 1 {
+            if let value = textField.text, !value.isEmpty { defaults.set(Double(value), forKey: "PetriIOUThreshold") } else { defaults.removeObject(forKey: "PetriIOUThreshold") }
+        } else if textField.tag == 2 {
+            if let value = textField.text, !value.isEmpty { defaults.set(Double(value), forKey: "PlaqueConfThreshold") } else { defaults.removeObject(forKey: "PlaqueConfThreshold") }
+        } else if textField.tag == 3 {
+            if let value = textField.text, !value.isEmpty { defaults.set(Double(value), forKey: "PlaqueIOUThreshold") } else { defaults.removeObject(forKey: "PlaqueIOUThreshold") }
+        } else if textField.tag == 4 {
+            if let value = textField.text, !value.isEmpty { defaults.set(Double(value), forKey: "PlaqueNMSIOUThreshold") } else { defaults.removeObject(forKey: "PlaqueNMSIOUThreshold") }
+        }
+        
+    }
 }
